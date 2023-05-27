35bfc84b02759b8e9b2d6a7f0b1a99539ea975ca
Donghwi Kim <ehdgnl0904@naver.com>
2023-05-27 18:08:27 +0900

    FEAT: IoT, AIoT Space 생성

---
+++ aiot_space/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiotSpaceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aiot_space'
---
+++ iot_space/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IotSpaceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'iot_space'
