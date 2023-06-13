8ddac9d8751ff4a12744d6ce6ea83388f6b42675
Daniel <clwnssla1@bible.ac.kr>
2023-06-13 17:59:26 +0900

    introduce.html 및 url 설정 비밀번호 변경 페이지 구현

--- SECS/views.py
+++ SECS/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # -- TemplateView
 class MainView(TemplateView):
     template_name = 'main.html'
+
+class IntroduceView(TemplateView):
+    template_name = 'introduce.html'
