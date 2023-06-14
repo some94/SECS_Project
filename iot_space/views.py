24c6df2bd18bcd44d116328709df98327f342646
Daniel <clwnssla1@bible.ac.kr>
2023-06-14 23:16:37 +0900

    iot_status 페이지 view 코드 수정

--- iot_space/views.py
+++ iot_space/views.py
@@ -10,33 +10,22 @@ import json
 #-- TemplateView
 class IoTStatus(View):
     def get(self, request):
-        return render(request, 'iot_status.html')
+        pointId = Point.objects.all()
+        return render('iot_status.html', pointId)
+        # return render(request, 'iot_status.html')
         # return render(request, 'iot_status.html', {'temperature': temp, 'detection': detect})
+    def ListFunc(request):
+        datas = SangTab.objects.all()
+        return render(request, 'list.html', {'sangpums': datas})
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
+    temp = Temperture(temp=data1)
+    temp.save()
     def post(self, request):
         json_data = json.loads(request.body)
         temp = json_data.get('temperature')
         detect = json_data.get('detection')
-        return render(request, 'iot_status.html', {'temperature': temp, 'detection': detect})
-
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        return HttpResponse("Method Not Allowed", status=405)
-
-# class IoTConsumption(View):
-#
-#     @method_decorator(csrf_exempt)
-#     def dispatch(self, request, *args, **kwargs):
-#         return super().dispatch(request, *args, **kwargs)
-#
-#     def post(self, request):
-#         json_data = json.loads(request.body.decode('utf-8'))
-#         temp = json_data.get('temperature')
-#         detect = json_data.get('detection')
-#         return render(request, 'iot_status.html', {'temperature': temp, 'detection': detect})
-#
-#     def http_method_not_allowed(self, request, *args, **kwargs):
-#         return HttpResponse("Method Not Allowed", status=405)
+        return JsonResponse(request, 'iot_status.html', {'temperature': temp, 'detection': detect})
