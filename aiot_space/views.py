84903bc9e7f5a7a70d61cedf46da6874a8ce9ee4
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-15 18:21:25 +0900

    post 세션값 get에서 받아오기

--- aiot_space/views.py
+++ aiot_space/views.py
@@ -1,14 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
+from aiot_space.models import DhtSensor, IrSensor
 
 import json
 
-#-- TemplateView
 class AIoTStatus(View):
     def get(self, request):
-        return render(request, 'aiot_status.html')
+        aiot_context = request.session.get['aiot_context']
+        if aiot_context:
+            context = {'aiot_context': aiot_context}
+            return render(request, 'aiot_status.html', context)
+        else:
+            return HttpResponse('Session value not found.')
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -19,6 +25,19 @@ class AIoTStatus(View):
         aiot_temperature = data['aiot_temperature']
         aiot_ir_state = data['aiot_ir_state']
         aiot_context = {'aiot_temperature': aiot_temperature, 'aiot_ir_state': aiot_ir_state}
-        print(aiot_context)
+        request.session['aiot_context'] = aiot_context
 
         return render(request, 'aiot_status.html', context=aiot_context, content_type='application/json')
+
+    # def get(self, request):
+    #     latest_dht_sensor = DhtSensor.objects.latest('timestamp')
+    #     latest_temperature = latest_dht_sensor.temperature
+    #     latest_ir_sensor = IrSensor.objects.latest('timestamp')
+    #     latest_status = latest_ir_sensor.status
+    #
+    #     context = {
+    #         'latest_temperature': latest_temperature,
+    #         'latest_status': latest_status
+    #     }
+    #
+    #     return render(request, 'aiot_status.html', context)
--- iot_space/views.py
+++ iot_space/views.py
@@ -1,13 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
+from iot_space.models import DhtSensor, IrSensor
 
 import json
 
 class IoTStatus(View):
     def get(self, request):
-        return render(request, 'iot_status.html')
+        iot_context = request.session.get['iot_context']
+        if iot_context:
+            context = {'iot_context': iot_context}
+            return render(request, 'iot_status.html', context)
+        else:
+            return HttpResponse('Session value not found.')
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -16,8 +23,21 @@ class IoTStatus(View):
     def post(self, request):
         data = json.loads(request.body.decode('utf-8'))
         iot_temperature = data['iot_temperature']
-        iot_iot_ir_state = data['iot_ir_state']
-        iot_context = {'iot_temperature': iot_temperature, 'iot_ir_state': iot_iot_ir_state}
-        print(iot_context)
+        iot_ir_state = data['iot_ir_state']
+        iot_context = {'iot_temperature': iot_temperature, 'iot_ir_state': iot_ir_state}
+        request.session['iot_context'] = iot_context
 
         return render(request, 'iot_status.html', context=iot_context, content_type='application/json')
+
+    # def get(self, request):
+    #     latest_dht_sensor = DhtSensor.objects.latest('timestamp')
+    #     latest_temperature = latest_dht_sensor.temperature
+    #     latest_ir_sensor = IrSensor.objects.latest('timestamp')
+    #     latest_status = latest_ir_sensor.status
+    #
+    #     context = {
+    #         'latest_temperature': latest_temperature,
+    #         'latest_status': latest_status
+    #     }
+    #
+    #     return render(request, 'iot_status.html', context)
